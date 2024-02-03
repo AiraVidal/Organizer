55767bfedb372ecdb57de20249b77f69cd12ffa0
AiraVidal <109344394+AiraVidal@users.noreply.github.com>
2024-02-03 09:44:48 +0000

    Update organizador.py
    o arquivo .py não é organizado

--- organizador.py
+++ organizador.py
@@ -5,25 +5,25 @@ def organizar_arquivos():
     # Obtenha o diretório atual do script
     diretorio_atual = os.getcwd()
 
-    # Pasta de origem é o diretório atual
+    # A pasta de origem é o diretório atual
     pasta_origem = diretorio_atual
 
-    # Pasta de destino é uma subpasta chamada "Organizados"
+    # A pasta de destino é uma subpasta chamada "Organizados"
     pasta_destino = os.path.join(diretorio_atual, "Organizados")
 
     # Certifique-se de que a pasta de destino exista
     if not os.path.exists(pasta_destino):
         os.makedirs(pasta_destino)
 
-    # Lista todos os arquivos na pasta de origem
+    # Liste todos os arquivos na pasta de origem
     arquivos = os.listdir(pasta_origem)
 
     for arquivo in arquivos:
         # Construa o caminho completo para o arquivo
         caminho_arquivo = os.path.join(pasta_origem, arquivo)
 
-        # Verifique se é um arquivo
-        if os.path.isfile(caminho_arquivo):
+        # Verifique se é um arquivo e não é o script atual
+        if os.path.isfile(caminho_arquivo) and arquivo != "organizador.py":
             # Obtenha a extensão do arquivo
             _, extensao = os.path.splitext(arquivo)
 
